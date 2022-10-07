f7ca1e77f5c08b201112f3ba5f6379a45dcb9014
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2022-10-07 11:52:37 -0500

    agrego mensaje

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+$("#carrito").click(function (event) {
+    $("#contenedor").load('carrito.html');
+    
+});
+var array=[];
+$("#comprar1").click(function(event){
+    let nombre=document.querySelector("#nombre").innerHTML;
+    console.log(nombre);
+  let nuevo =array.push(nombre);
+})
+$("#carta").click(function(event){
+    ejecutarApi('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+});
+function ejecutarApi(link){
+    fetch(url)
+        .then(response => response.json())
+        .then(data => mostrarData(data))
+        .catch(error => console.log(error))
+
+        const mostrarData = (data) => {
+            var drinks=data.drinks;
+            var rta="";
+            
+            for(let i=0;i<drinks.length;i++){
+                if (drinks[i] != undefined){
+                    rta += 
+                   
+                    
+                    ` <div class="container">
+                    <li class="list-group-item">
+                     <img class="imgCoctel"  height="150" width="150" src="${drinks[i].strDrinkThumb}"" > <h2 class="nombreCoctel" >${drinks[i].strDrink}</h2> </li>' `;
+                }
+            }
+            document.querySelector("#contenedor").innerHTML = rta;
+        }
+} 
