bd34d0c179eb7cd1aa9822e51bf091ef7c14319b
CIRO QUINTERO <ciroalfonsoqude@ufps.edu.co>
2022-10-07 18:32:47 -0500

    agrego carrito

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
-
+var array=[];
 $("#carrito").click(function (event) {
     $("#contenedor").load('carrito.html');
+    fulltable();
     
 });
 
-var array=[];
+
 $(".comprar").click(function(event){
     let nombre=document.querySelector(".nombre").innerHTML;
     console.log(nombre);
@@ -15,13 +16,39 @@ $(".comprar").click(function(event){
 $("#lista").click(function(event){
     ejecutarApi();
 });
+function agregar(nombre){
+    //let nombre=document.querySelector(".nombreCoctel").innerHTML;
+    console.log(nombre);
+  let number =array.push(nombre);
+}
 
-function getData(){
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-};
+function fulltable(){
+    //var table=document.querySelector("#tabla");
+    for(let i=0;i<array.length;i++){
+
+        var fila = '<tr> <td> ' + i+ '</td><td>' + array[i] + '</td><td>' + 20000+ '</td></tr>'
+
+    $("#tabla").append(fila);
+       /*
+        let nomb=array[i];
+        let row = table.insertRow();
+
+        let cell_index =row.insertCell();
+        cell_index.innerHTML=(i+1);
+
+        let cell_nombre=row.insertCell();
+        cell_nombre.innerHTML=nomb;
+
+        let cell_precio=row.insertCell();
+        cell_precio.innerHTML=20000;*/
+    }
+    
+
+               
+}
+
+
+
 
 function ejecutarApi(){
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
@@ -39,7 +66,7 @@ function ejecutarApi(){
                    
                     
                     ` <div class="col" >
-                     <img class="imgCoctel"  height="150" width="150" src="${drinks[i].strDrinkThumb}"" > <h2 class="nombreCoctel" >${drinks[i].strDrink}</h2><button type="button" class="btn btn-dark" >COMPRAR</button> <br></div>  `;
+                     <img class="imgCoctel"  height="150" width="150" src="${drinks[i].strDrinkThumb}" > <h1 class="nombreCoctel" >${drinks[i].strDrink}</h1><button type="button" class="btn btn-success"  onclick="agregar('${drinks[i].strDrink}')" >COMPRAR</button> <br></div>  `;
                 }
             }
             document.querySelector("#contenedor").innerHTML = rta;
